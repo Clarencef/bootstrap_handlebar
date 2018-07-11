2f47121cda17bc678ea99ec2bd1f255d99cf6d2d
albert fang <hr00090241@gmail.com>
2018-07-11 11:23:49 +0800

    change project structure

---
+++ config/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const outputDirectory = 'dist';
+
+module.exports = {
+  entry: path.resolve(__dirname, '..','src/client/index.js'),
+  output: {
+    path: path.resolve(__dirname, '..', outputDirectory),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', 'public/index.html'),
+    })
+  ]
+};
